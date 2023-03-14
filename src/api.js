f005000ec0a92fe4d76e299ee2428cc92017f1e4
Michael Greenan <mikegreenan98@gmail.com>
2023-03-14 12:05:55 +0000

    06 minor change after review

--- src/api.js
+++ src/api.js
@@ -5,16 +5,12 @@ const newsApi = axios.create({
 });
 
 export const fetchArticles = (filter) => {
-//   const filterObject = {params: filter };
-  // NOTE - very handy - axios get 2nd param can contain all the queries !
   return newsApi.get("/articles").then((res) => {
-    // console.log(res.data.articles);
     return res.data.articles;});
 };
 
 export const fetchOneArticle = (article_id) => {
     return newsApi.get(`/articles/${article_id}`).then((res) => {
-      // console.log(res.data.article[0]);
       return res.data.article[0];});
 };
   
