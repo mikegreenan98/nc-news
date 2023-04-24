41771b72ae665654d494021f6fe3b7b58247b07d
Michael Greenan <mikegreenan98@gmail.com>
2023-04-24 16:55:04 +0100

    fixed render issue

--- src/components/ArticlesDisplay.jsx
+++ src/components/ArticlesDisplay.jsx
@@ -1,6 +1,6 @@
 import {useState, useEffect} from "react";
 import { fetchArticles } from "../api";
-// import RenderArticlesList from "./RenderArticlesList"
+import RenderArticlesList from "./RenderArticlesList"
 import ErrorAPI from "./ErrorAPI";
 import { Link } from "react-router-dom";
 const uuid = require('uuid');
@@ -37,40 +37,7 @@ const ArticlesDisplay = ({currentFilter}) => {
 
     if(isLoading) {return <h2>Loading articles...</h2>}
 
-    // have tried everthing to force the render when changes topics but still get occasional
-    // time when does not refresh - seems better when I put the renderartcilelist code here..
-    // return <RenderArticlesList list={articlesList}/>
-
-    return(
-        <div>
-            <ul>
-                {articlesList.map((article,index) => {
-                    return(
-                    <div key={uuid.v4()} className="article">
-                    {/* <li key={article.article_id} className="articleCard"> */}
-                    {/* <li key={index} className="articleCard"> */}
-                    {/* <li key={article.article_id+list.length*10} className="articleCard"> */}
-                    <li key={uuid.v4()} className="articleCard">
-                        <p key={article.title+uuid.v4()}>Title: {article.title}</p>
-                        <p key={article.article_id+uuid.v4()}>Id: {article.article_id}</p>
-                        <p key={article.author+uuid.v4()}>Author: {article.author}</p>
-                        <img key={article.article_img_url+uuid.v4()} className="articleImage" src={article.article_img_url} alt=""></img>
-                        <p key={article.topic+uuid.v4()}>Topic: {article.topic}</p>
-                        <p key={article.votes+uuid.v4()}>Votes: {article.votes}</p>
-                        <p key={article.created_at+uuid.v4()}>Date: {new Date(article.created_at).toDateString().substring(4,)}</p>
-                        <Link to={`/articles/${article.article_id}`} key={uuid.v4()}>
-                        <button className = "ButtonViewArticle">View this article</button>
-                        </Link>
-                    </li>
-                    </div>);
-                })}
-            </ul>
-        </div>
-    )
-
-
-
-
+    return <RenderArticlesList list={articlesList}/>
 }
 
 export default ArticlesDisplay;
